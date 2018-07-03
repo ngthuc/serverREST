b5d9a60cbdce150650c36655e31d040c875c264e
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-07-03 08:49:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require('express');
 var bodyparser = require('body-parser');
 
 var connection = require('./Dbconnection');
-var routes = require('./routes');
+var routesSV = require('./routesSV');
 
 var app = express();
 app.use(bodyparser.urlencoded({extended: true})); //support x-www-form-urlencoded
 app.use(bodyparser.json());
 
-app.use('/sinhvien',routes);
+app.use('/sinhvien',routesSV);
 
 var server = app.listen(10110, function() {
   console.log('Server listening on port ' + server.address().port);
